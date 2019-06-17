56c3e518a215da7944462275717e6546fa7218d1
felipe <fcpereira@dcomp.ufs.br>
2019-06-17 20:17:06 -0300

    Reorganização das instâncias para re-execução

--- script_standard_groupC.sh
+++ script_standard_groupC.sh
@@ -2,5 +2,5 @@
 
 for filename in instances/groupC/*; do
 	echo "$filename"
-	g++ -o grasp-pa grasp-pa.cpp && ./grasp-pa 1 100 2 0.05 1800 "$filename" output/output_standard_groupC.csv >> logs/log_standard_groupC.txt
+	g++ -o grasp-pa grasp-pa.cpp && ./grasp-pa 1 100 2 0.05 3600 "$filename" output/output_standard_groupC.csv >> logs/log_standard_groupC.txt
 done
