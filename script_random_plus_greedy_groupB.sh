57bdb306e24ed705eab04120025a2754ab0ec557
felipe <fcpereira@dcomp.ufs.br>
2019-06-18 08:37:46 -0300

    Reorganização para instâncias menores

--- script_random_plus_greedy_groupB.sh
+++ script_random_plus_greedy_groupB.sh
@@ -2,5 +2,5 @@
 
 for filename in instances/groupB/*; do
 	echo "$filename"
-	g++ -o grasp-pa grasp-pa.cpp && ./grasp-pa 2 1 2 1 1800 "$filename" output/output_random_plus_greedy_groupB.csv >> logs/log_random_plus_greedy_groupB.txt
+	g++ -o grasp-pa grasp-pa.cpp && ./grasp-pa 2 1 2 1 600 "$filename" output/output_random_plus_greedy_groupB.csv >> logs/log_random_plus_greedy_groupB.txt
 done
--- script_standard_groupB.sh
+++ script_standard_groupB.sh
@@ -2,5 +2,5 @@
 
 for filename in instances/groupB/*; do
 	echo "$filename"
-	g++ -o grasp-pa grasp-pa.cpp && ./grasp-pa 1 1 2 1 1800 "$filename" output/output_standard_groupB.csv >> logs/log_standard_groupB.txt
+	g++ -o grasp-pa grasp-pa.cpp && ./grasp-pa 1 1 2 1 600 "$filename" output/output_standard_groupB.csv >> logs/log_standard_groupB.txt
 done
